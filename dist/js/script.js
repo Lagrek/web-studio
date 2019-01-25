11070a4c8bc7c188cb8c12b399c79cf71a958427
Skorpzmey <ilyushin.51072@gmail.com>
2019-01-25 20:42:06 +0300

    add prlx

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,5 +1,12 @@
-
-
-  $(document).ready(function(){
+$(document).ready(function(){
     $('.sidenav').sidenav();
   });
+
+$(window).bind('scroll',function(e){
+    parallaxScroll();
+});
+ 
+function parallaxScroll(){
+    var scrolled = $(window).scrollTop();
+    $('#parallax').css('top',(0-(scrolled*.70))+'px');
+}
